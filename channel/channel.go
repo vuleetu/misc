ef1431774a44886b4b68b2a057af61f2221c2b54
Fisher <fisher@yun.io>
2013-06-14 15:44:13 +0800

    Add infinite channel

---
+++ channel/channel.go
@@ -0,0 +1,51 @@
+package channel
+
+import (
+    "reflect"
+    "container/list"
+)
+
+func NewByteChan() (<-chan byte, chan<- byte) {
+    r, w := NewChan(byte(0))
+    return r.(chan byte), w.(chan byte)
+}
+
+func NewChan(typ interface{}) (interface{}, interface{}) {
+    rc := reflect.MakeChan(reflect.ChanOf(reflect.BothDir, reflect.TypeOf(typ)), 0)
+    wc := reflect.MakeChan(reflect.ChanOf(reflect.BothDir, reflect.TypeOf(typ)), 0)
+
+    go loop(rc, wc)
+
+    return rc.Interface(), wc.Interface()
+}
+
+func loop(rc reflect.Value, wc reflect.Value) {
+    readCase := reflect.SelectCase{}
+    readCase.Dir = reflect.SelectRecv
+    readCase.Chan = wc
+
+    writeCase := reflect.SelectCase{}
+    writeCase.Dir = reflect.SelectSend
+    writeCase.Chan = rc
+
+    cases := []reflect.SelectCase{readCase, writeCase}
+
+    l := list.New()
+
+    for {
+        tc := cases[:1]
+        if l.Len() > 0 {
+            writeCase.Send = reflect.ValueOf(l.Front().Value)
+            cases[1] = writeCase
+            tc = cases[:2]
+        }
+
+
+        idx, v, ok := reflect.Select(tc)
+        if idx == 0 && ok {
+            l.PushBack(v.Interface())
+        } else if idx == 1 {
+            l.Remove(l.Front())
+        }
+    }
+}
